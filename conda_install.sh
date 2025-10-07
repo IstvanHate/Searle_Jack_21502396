36538b03750af68b121bc43433bef7097c64b138
IstvanHate <jack.searle.sus@example.com>
2025-10-08 04:25:26 +0800

    trained task2 YOLO model, Train_YOLO_locally notebook now working, added SSH key to github (not in repo lol)

---
+++ conda_install.sh
@@ -0,0 +1,8 @@
+# Download Miniconda installer
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh  # Linux
+
+# Run the installer
+bash Miniconda3-latest-*.sh
+
+# Follow prompts, then restart shell
+source ~/.bashrc   # or `source ~/.zshrc`
